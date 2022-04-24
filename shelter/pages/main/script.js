4fc4cfaa860d3b96c4e31ab48b8ac9a136978479
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-04-25 00:07:48 +0300

    feat: add pop-up window to main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -12,8 +12,21 @@ const BODY = document.querySelector('body');
 const SLIDER_ITEMS = document.querySelector('.slider-items');
 const SLIDER_ITEM_LEFT = document.querySelector('.slider-item.left');
 const SLIDER_ITEM_CENTER = document.querySelector('.slider-item.center');
+let cards;
 const SLIDER_ITEM_RIGHT = document.querySelector('.slider-item.right');
+const MODAL_WINDOW = document.querySelector('.modal-overlay');
+const MODAL_CLOSE_BUTTON = document.querySelector('.pop-up-close-button');
+const POP_UP_IMAGE = document.querySelector('.pop-up-image');
+const POP_UP_NAME = document.querySelector('.pop-up-name');
+const POP_UP_TYPE = document.querySelector('.pop-up-type');
+const POP_UP_TEXT = document.querySelector('.pop-up-text');
+const POP_UP_AGE = document.querySelector('.pop-up-age');
+const POP_UP_INOCULATIONS = document.querySelector('.pop-up-inoculations');
+const POP_UP_DISEASES = document.querySelector('.pop-up-diseases');
+const POP_UP_PARASITES = document.querySelector('.pop-up-parasites');
 const CARDS_QUANTITY = 3;
+
+// console.log(MODAL_WINDOW);
 
 await DATA.forEach(item => petsArray.push(item));
 
@@ -52,16 +65,10 @@ const toggleNavigation = () => {
   BODY.classList.toggle('no-scroll');
 }
 
-const initPage = () => {
-  currentCardsArray = generateSliderCardsNumbers(CARDS_QUANTITY, [petsArray.length]);
-  SLIDER_ITEM_CENTER.innerHTML = '';
-  SLIDER_ITEM_CENTER.innerHTML = generateCards(currentCardsArray);
-}
-
 BURGER_ICON.addEventListener('click', toggleNavigation);
 
 MENU.addEventListener('click', () => {
-  if (event.target.classList.contains('nav-menu-item'))
+  if (event.target.classList.contains('nav-menu-item') && MENU.classList.contains('active'))
     toggleNavigation();
 });
 
@@ -88,8 +95,35 @@ const moveSliderRight = () => {
    SLIDER_ITEMS.classList.add("transition-right");
 };
 
+const generatePopUp = (petId) => {
+  POP_UP_IMAGE.innerHTML = `<img src="${petsArray[petId]["img"]}" alt="pet">\n`;
+  POP_UP_NAME.innerHTML = `${petsArray[petId]["name"]}`;
+  POP_UP_TYPE.innerHTML = `${petsArray[petId]["type"]} - ${petsArray[petId]["breed"]}`;
+  POP_UP_TEXT.innerHTML = `${petsArray[petId]["description"]}`;
+  POP_UP_AGE.innerHTML = `${petsArray[petId]["age"]}`;
+  POP_UP_INOCULATIONS.innerHTML = `${petsArray[petId]["inoculations"].join(', ')}`;
+  POP_UP_DISEASES.innerHTML = `${petsArray[petId]["diseases"].join(', ')}`;
+  POP_UP_PARASITES.innerHTML = `${petsArray[petId]["parasites"].join(', ')}`;;
+}
+
+const showPopUp = (event) => {
+  generatePopUp(event.currentTarget.dataset.pid);
+  BODY.classList.add('no-scroll');
+  MODAL_WINDOW.classList.add('active');
+}
+
 BUTTON_LEFT.addEventListener('click', moveSliderLeft);
 BUTTON_RIGHT.addEventListener('click', moveSliderRight);
+MODAL_WINDOW.addEventListener('click', (event) => {
+  if(event.target === MODAL_WINDOW) {
+    MODAL_WINDOW.classList.remove('active');
+    BODY.classList.remove('no-scroll');
+  }
+});
+MODAL_CLOSE_BUTTON.addEventListener('click', (event) => {
+  MODAL_WINDOW.classList.remove('active');
+  BODY.classList.remove('no-scroll');
+});
 
 SLIDER_ITEMS.addEventListener('animationend', (event) => {
  let changedSliderItem;
@@ -101,8 +135,18 @@ SLIDER_ITEMS.addEventListener('animationend', (event) => {
    changedSliderItem = SLIDER_ITEM_RIGHT;
  }
  SLIDER_ITEM_CENTER.innerHTML = changedSliderItem.innerHTML;
+ cards = document.querySelectorAll('.slider-item.center .card');
+ cards.forEach(card => card.addEventListener('click', showPopUp));
  BUTTON_LEFT.addEventListener('click', moveSliderLeft);
  BUTTON_RIGHT.addEventListener('click', moveSliderRight);
 });
+
+const initPage = () => {
+  currentCardsArray = generateSliderCardsNumbers(CARDS_QUANTITY, [petsArray.length]);
+  SLIDER_ITEM_CENTER.innerHTML = '';
+  SLIDER_ITEM_CENTER.innerHTML = generateCards(currentCardsArray);
+  cards = document.querySelectorAll('.slider-item.center .card');
+  cards.forEach(card => card.addEventListener('click', showPopUp));
+}
 
 initPage();
