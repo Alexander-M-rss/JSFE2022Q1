91e7c921c256c57b242844ac9ee8d373230baa3a
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-06-30 17:46:45 +0300

    build: add API key, install initial dependecies

--- news-TS/src/components/controller/appLoader.js
+++ news-TS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '7af2c632159747118078af9085bce66f', // получите свой ключ https://newsapi.org/
         });
     }
 }
