c35cbd344cb1a09a03277f53f574d3f143da3cf5
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-07-01 20:43:08 +0300

    refactor: add src/types/index.ts, migrate news.js to news.ts

---
+++ news-TS/src/types/index.ts
@@ -0,0 +1,18 @@
+// export interface IResponse {
+//   status: string; //?
+//
+// }
+
+export interface Article {
+    source: {
+        id: string;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
