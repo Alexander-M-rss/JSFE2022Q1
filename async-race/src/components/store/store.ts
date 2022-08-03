456a1a20f5d7a12e9591555da7458935e0a05162
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-08-03 15:28:35 +0300

    feat: add store for application state data

---
+++ async-race/src/components/store/store.ts
@@ -0,0 +1,43 @@
+import {
+  getCars, getWinners, Car, Winner, SortModes, SortOrders,
+} from '../api/api';
+
+interface Storage {
+  carsPage: number;
+  cars: Array<Car>;
+  carsNumber: number;
+  winnersPage: number;
+  winners: Array<Winner>;
+  winnersNumber: number;
+  animation: { [id: number]: number };
+  view: 'garage' | 'winners';
+  sortBy: SortModes;
+  sortOrder: SortOrders;
+  CARS_PER_PAGE: number;
+  WINNERS_PER_PAGE: number;
+}
+
+const store: Storage = {
+  carsPage: 1,
+  cars: [],
+  carsNumber: 0,
+  winnersPage: 1,
+  winners: [],
+  winnersNumber: 0,
+  animation: {},
+  view: 'garage',
+  sortBy: null,
+  sortOrder: null,
+  CARS_PER_PAGE: 7,
+  WINNERS_PER_PAGE: 10,
+};
+
+const { cars, carsNumber } = await getCars(store.carsPage, store.CARS_PER_PAGE);
+const { winners, winnersNumber } = await getWinners(store.winnersPage, store.WINNERS_PER_PAGE);
+
+store.cars = cars;
+store.carsNumber = carsNumber;
+store.winners = winners;
+store.winnersNumber = winnersNumber;
+
+export default store;
--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,16 +1,5 @@
 import './style.css';
 
-import * as api from './components/api/api';
+import store from './components/store/store';
 
-(async () => {
-  api.createCar('honda', 'blue');
-  api.createCar('toyota', 'red');
-  console.log(await api.getWinners(1, 7));
-  console.log(await api.getWinner(3));
-  console.log(await api.createWinner(6, 5.5, 2));
-  console.log(await api.getWinners(1, 7));
-  console.log(await api.updateWinner(6, 5, 4));
-  console.log(await api.getWinners(1, 7));
-  console.log(await api.deleteWinner(6));
-  console.log(await api.getWinners(1, 7));
-})();
+console.log(store);
